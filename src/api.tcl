f14d13acc57a390cbeed55fc90a1e2efa5d94364
dcp1990 <dcp1990>
2005-01-19 04:08:44 +0000

    The start of callbacks.

--- src/api.tcl
+++ src/api.tcl
@@ -1,10 +1,16 @@
 #api
 namespace eval beartkat {
 	namespace export docommand
+	variable cbs
+	namespace export togglelight
+	namespace export register
 	proc docommand {cmd} {
 		return [scommand $cmd]
 	}
 	proc togglelight {} {
 		return [scommand "KEY08"]
+	}
+	proc register {event callback} {
+		lappend cbs $event $callback
 	}
 }
